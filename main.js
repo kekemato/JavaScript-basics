0ca67d2654f965bcc75291706057f8e29c52c16a
kekemato <magdalena.brzozowska94@wp.pl>
2018-10-08 14:38:36 +0200

    Basic mathematical operators exercise

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ alert("Hello again");
 const name = "Magda";
 var age = 24;
 let adress = "Lublin" 
+
+age = age - 1;
+adress = "Lublin" + " Poland";
+
+/* Basic mathematical operators:
+= - assign
++ - add numbers and strings
+- - substract numbers
+* - multiply numbers
+/ - divide numbers */
