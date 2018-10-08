c8cc99a0e63023cb60b8b5dae11821af0f38eaa2
kekemato <magdalena.brzozowska94@wp.pl>
2018-10-08 15:48:08 +0200

    comparison operators exercise

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var youLikeBoardGames = true;
 var number = 10;
+var x = "5";
 
 if (youLikeBoardGames = true){
     document.write("You love board games!");
@@ -16,4 +17,18 @@ else if (number < 9){
 }
 else if (number == 9){
     document.write(" <br> Number is equal to 9");
+}
+
+if (x !== 5){
+    document.write("<br> x isn't equal to 5 because it's a string not an integer.");
+}
+else if (x === 5){
+    document.write("<br> x is equal to 5 and it's an integer.");
+}
+
+if (x >= 5){
+    document.write("<br> x is greater than or equal to 5");
+}
+else if (x < 5){
+    document.write("<br> x is smaller than 5");
 }
