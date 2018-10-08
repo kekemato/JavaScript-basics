f5c42ca39905d45eeb5dbd81672305f73c96d6a7
kekemato <magdalena.brzozowska94@wp.pl>
2018-10-08 14:49:27 +0200

    shorthand method for mathematical operator and incrementation and decrementation

--- main.js
+++ main.js
@@ -9,12 +9,16 @@ const name = "Magda";
 var age = 24;
 let adress = "Lublin" 
 
-age = age - 1;
-adress = "Lublin" + " Poland";
+age -= 1;
+adress += "Poland";
 
 /* Basic mathematical operators:
 = - assign
 + - add numbers and strings
 - - substract numbers
 * - multiply numbers
-/ - divide numbers */
+/ - divide numbers 
+++var - increment
+var++
+--var - decrement
+var-- */
