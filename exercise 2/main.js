21d438caae26f0bc7bbd6984238fea6e9afa4151
kekemato <magdalena.brzozowska94@wp.pl>
2018-10-09 13:44:48 +0200

    making new objects exercise

---
+++ exercise 2/main.js
@@ -0,0 +1,29 @@
+var myCar = new Object();
+myCar.driver = "Magda";
+myCar.maxSpeed = 150;
+myCar.color = "red";
+
+myCar.drive = function(){
+    console.log("I'm driving");
+}
+myCar.distanceTraveled = function(speed,time){
+    console.log(speed * time);
+}
+
+myCar.distanceTraveled(100, 2.5);
+
+var myCar2 = {
+    driver: "Patryk",
+    maxSpeed: 100,
+    color: "black",
+
+    drive: function(){
+        console.log("I'm driving");
+    },
+
+    distanceTraveled: function(speed,time){
+        console.log(speed * time);
+    }
+};
+
+myCar2.drive();
