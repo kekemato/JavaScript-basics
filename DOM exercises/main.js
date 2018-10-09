34610b0387a59208982520cfe0750bc1e941138f
kekemato <magdalena.brzozowska94@wp.pl>
2018-10-09 16:41:18 +0200

    Traversing the DOM

---
+++ DOM exercises/main.js
@@ -0,0 +1,7 @@
+var mainTitle = document.getElementsByTagName("h1");
+console.log(mainTitle);
+mainTitle.innerHTML = "I changed this title via js!";
+var text = document.getElementsByClassName("text");
+console.log(text);
+var uniqueParagraph = document.getElementById("unique-paragraph");
+console.log(uniqueParagraph);
