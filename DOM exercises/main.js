ebf4ae48d3cff66c3f0ee74ae34866ae727c3e85
kekemato <magdalena.brzozowska94@wp.pl>
2018-10-09 18:17:53 +0200

    getAttribute and setAttribute methods

--- DOM exercises/main.js
+++ DOM exercises/main.js
@@ -1,8 +1,10 @@
 var mainTitle = document.getElementsByTagName("h1");
 console.log(mainTitle);
+mainTitle[0].setAttribute("class", "titles");
 mainTitle.innerHTML = "I changed this title via js!";
 var text = document.getElementsByClassName("text");
 console.log(text);
 var uniqueParagraph = document.getElementById("unique-paragraph");
 console.log(uniqueParagraph.textContent);
 uniqueParagraph.textContent = "This is a new awesome text."
+console.log(uniqueParagraph.getAttribute("id"));
