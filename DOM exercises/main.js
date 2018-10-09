593a4cfd9270d12ca0403a0ba5f1fa31343b1ca5
kekemato <magdalena.brzozowska94@wp.pl>
2018-10-09 18:30:49 +0200

    changing css styles with js

--- DOM exercises/main.js
+++ DOM exercises/main.js
@@ -2,9 +2,12 @@ var mainTitle = document.getElementsByTagName("h1");
 console.log(mainTitle);
 mainTitle[0].setAttribute("class", "titles");
 mainTitle.innerHTML = "I changed this title via js!";
+
 var text = document.getElementsByClassName("text");
 console.log(text);
 var uniqueParagraph = document.getElementById("unique-paragraph");
 console.log(uniqueParagraph.textContent);
 uniqueParagraph.textContent = "This is a new awesome text."
 console.log(uniqueParagraph.getAttribute("id"));
+uniqueParagraph.setAttribute("style", "color:red;");
+uniqueParagraph.style.color="blue";
