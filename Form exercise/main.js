4a3ebb5be64cdfefda39f30f0ba21787cccad5eb
kekemato <magdalena.brzozowska94@wp.pl>
2018-10-09 22:03:55 +0200

    deleted unnecessary semicolons

--- Form exercise/main.js
+++ Form exercise/main.js
@@ -2,10 +2,10 @@ window.onload = function(){
     var myForm = document.forms.myForm;
 
 myForm.name.onfocus = function(){
-    myForm.name.style.border = "1px solid red;";
+    myForm.name.style.border = "1px solid red";
 }
 
 myForm.name.onblur = function(){
-    myForm.name.style.border = "none;";
+    myForm.name.style.border = "none";
 }
 }
